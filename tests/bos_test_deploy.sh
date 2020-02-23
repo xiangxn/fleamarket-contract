bb357a3937047547c5fd98a05bd99c1ac8f9b3bc
necklace <xiangxn@163.com>
2020-02-24 00:38:58 +0800

    add bos test deploy sh

---
+++ tests/bos_test_deploy.sh
@@ -0,0 +1,26 @@
+export BOS_API_URL="https://api-bostest.blockzone.net"
+export SYMBOL="BOS"
+export USE_OLD_RPC="--use-old-rpc"
+# buy resources
+#cleos -u $BOS_API_URL get currency balance eosio.token bitsfleamain BOS
+cleos -u $BOS_API_URL system buyram bitsfleamain bitsfleamain "2000 ${SYMBOL}" $USE_OLD_RPC
+#cleos -u $BOS_API_URL system delegatebw bitsfleamain bitsfleamain "10 ${SYMBOL}" "400 ${SYMBOL}" $USE_OLD_RPC
+# deploy
+cleos wallet unlock -n bostest  --password PW5KajM5kmn6YSckdg3hWregB1bGQ3VMKeUpSyHDMXuukZZnTiyFj
+cleos -u $BOS_API_URL set contract bitsfleamain ./build/bitsfleamain -p bitsfleamain $USE_OLD_RPC &&
+cleos -u $BOS_API_URL set account permission bitsfleamain active '{"threshold": 1,"keys": [{"key": "EOS6VqjyRiJZWUDFErcnX7CkhDkkBKjaZCsLfGJXPJEEe5DNbW2KW","weight": 1}],"accounts": [{"permission":{"actor":"bitsfleamain","permission":"eosio.code"},"weight":1}]}' owner -p bitsfleamain $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain init '[]' -p bitsfleamain $USE_OLD_RPC &&
+# reg user
+cleos -u $BOS_API_URL push action bitsfleamain reguser '[reviewer1111,"reviewer1111","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B492","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B492",0]' -p bitsfleamain $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain reguser '[reviewer1112,"reviewer1112","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B493","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B493",0]' -p bitsfleamain $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain reguser '[reviewer1113,"reviewer1113","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B494","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B494",0]' -p bitsfleamain $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain reguser '[bitsflea1111,"bitsflea1111","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B495","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B495",0]' -p bitsfleamain $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain reguser '[bitsflea1112,"bitsflea1112","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B496","05FBED483A974456708EF12FCF3D278276E982234D775ECEE0DFA5CB49B8B496",0]' -p bitsfleamain $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain appreviewer '[0,"reviewer1111"]' -p reviewer1111 $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain appreviewer '[1,"reviewer1112"]' -p reviewer1112 $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain appreviewer '[2,"reviewer1113"]' -p reviewer1113 $USE_OLD_RPC &&
+# vote reviewer
+cleos -u $BOS_API_URL push action bitsfleamain votereviewer '[3,"bitsflea1111",0,true]' -p bitsflea1111 $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain votereviewer '[3,"bitsflea1111",1,true]' -p bitsflea1111 $USE_OLD_RPC &&
+cleos -u $BOS_API_URL push action bitsfleamain votereviewer '[3,"bitsflea1111",2,true]' -p bitsflea1111 $USE_OLD_RPC &&
+cleos -u $BOS_API_URL get table bitsfleamain bitsfleamain global
---
+++ tests/btstest.sh
@@ -0,0 +1,41 @@
+cleos push action bitsfleamain issue '[ "player1", "10000.00000 BTS", "memo" ]' -p bitsfleamain &&
+
+cleos push action bitsfleamain publish '[0,{"pid":0,"uid":0,"title":"title test","description":"description 测试","photos":"photos 测试","category":0,"status":0,"is_new":false,"is_returns":true,"reviewer":0,"sale_method":0,"price":"2.09000 BTS","transaction_method":1,"postage":"0.01000 BTS","position":"位置","release_time":"2020-01-20T00:00:00"},{"id":0,"pid":0,"security":"1.00000 BTS","markup":"1.00000 BTS","current_price":"1.00000 BTS","auction_times":0,"last_price_user":0,"start_time":"2020-01-20T00:00:00","end_time":"2020-01-22T00:00:00"}]' -p bitsfleamain &&
+
+cleos push action bitsfleamain review '[1,"reviewer",1,false,"good product"]' -p reviewer &&
+
+cleos push action bitsfleamain placeorder '[2,"player1",1]' -p player1 &&
+
+cleos push action bitsfleamain bindaddr '[0,"player","5,BTS","xiangxn"]' -p player &&
+
+cleos push action bitsfleamain issue '[ "player", "1000.00000 BTS", "memo" ]' -p bitsfleamain &&
+
+cleos push action bitsfleamain issue '[ "fleagateways", "1000.00000 BTS", "memo" ]' -p bitsfleamain &&
+
+cleos get table bitsfleamain bitsfleamain orders 
+
+# pay : order id 在上一行的输出中取
+# cleos transfer player1 bitsfleamain "2.10000 BTS" "payorder:36893488153296537970" -p player1 -c bitsfleamain
+# cleos get currency balance bitsfleamain player BTS
+# cleos get currency balance bitsfleamain player1 BTS
+# cleos get currency balance bitsfleamain bitsfleamain BTS
+
+
+
+# cleos push action bitsfleamain shipment '[0,"player","36893488153296537970","229988222"]' -p player
+# cleos push action bitsfleamain conreceipt '[2,"player1","36893488153296537970"]' -p player1
+# cleos push action bitsfleamain returns '[2,"player1","36893488153296537970","商品与描述不符"]' -p player1
+# cleos push action bitsfleamain reshipment '[2,"player1","36893488153296537970","229988223"]' -p player1
+# cleos push action bitsfleamain reconreceipt '[0,"player","36893488153296537970"]' -p player
+
+# cleos transfer player bitsfleamain "0.10000 BTS" "withdraw:0x165a3e5e0100000" -p player -c bitsfleamain
+# cleos push action bitsfleamain closesettle '[0]' -p bitsfleamain
+# cleos get table bitsfleamain bitsfleamain othersettle
+
+cleos get currency balance bitsfleamain player BTS &&
+cleos get currency balance bitsfleamain player1 BTS &&
+cleos get currency balance bitsfleamain reviewer BTS &&
+cleos get currency balance bitsfleamain fleagateways BTS &&
+cleos get currency balance bitsfleamain bitsfleamain BTS
+
+# cleos transfer player bitsfleamain "2.00000 BTS" "withdraw:0" -p player -c bitsfleamain
